625b14df083232fc15060fd34403101f86541873
mohammedtakbir <takbirhossain678@gmail.com>
2022-10-25 18:51:57 +0600

    completed router setup & add more routes

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer/Footer';
+import Navbar from '../components/Navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
-            
+            <Navbar />
+            <div className='container mx-auto'>
+                <Outlet />
+            </div>
+            <Footer />
         </div>
     );
 };
---
+++ src/routers/routes.js
@@ -0,0 +1,45 @@
+import { createBrowserRouter } from "react-router-dom";
+import Blog from "../components/Blog/Blog";
+import Courses from "../components/Courses/Courses";
+import FAQ from "../components/FAQ/FAQ";
+import Home from "../components/Home/Home";
+import Login from "../components/Login/Login";
+import Signup from "../components/Signup/Signup";
+import Main from "../layout/Main";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            },
+            {
+                path: '/home',
+                element: <Home />
+            },
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+            {
+                path: '/courses',
+                element: <Courses />
+            },
+            {
+                path: '/faq',
+                element: <FAQ />
+            },
+            {
+                path: '/blog',
+                element: <Blog />
+            },
+        ]
+    }
+])
