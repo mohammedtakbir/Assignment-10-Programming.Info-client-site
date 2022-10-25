17d14827ff4341101a9cc590276e691b5e7c0eab
mohammedtakbir <takbirhossain678@gmail.com>
2022-10-25 20:42:16 +0600

    login UI added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,67 @@
 import React from 'react';
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
+
+
+    
+    const handleLogIn = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        
+
+
+
+    }
+
+
     return (
         <div>
-            <h2>login</h2>
+            <h3 className='text-center mt-14 font-bold mb-6'>Log in to your Programming.Info account</h3>
+            <hr className='w-[400px] mx-auto' />
+            <div className='flex justify-center mt-7'>
+                <div>
+                    <button className='flex items-center bg-slate-100 hover:bg-slate-200 py-3 px-4 mb-3 rounded-md text-lg w-[300px]'>
+                        <span className='text-2xl'><FaGithub /></span>
+                        <span className='font-medium ml-3'>Continue with Github</span>
+                    </button>
+                    <button className='flex items-center bg-slate-100 hover:bg-slate-200 py-3 px-4 rounded-md text-lg w-[300px]'>
+                        <span className='text-2xl'><FcGoogle /></span>
+                        <span className='font-medium ml-3'>Continue with Google</span>
+                    </button>
+                    <p className='text-center text-lg font-bold mt-3'>or</p>
+                    <form onSubmit={handleLogIn}>
+                        <div className='mb-4'>
+                            <label
+                                htmlFor=""
+                                className='block text-lg font-medium mb-1'>Email</label>
+                            <input
+                                className='border w-[300px] border-[#8c99ab] py-2 px-3 rounded-md text-lg'
+                                type="email"
+                                name="email"
+                                id=""
+                                placeholder='your Email address' />
+                        </div>
+                        <div className='mb-4'>
+                            <label
+                                htmlFor=""
+                                className='block text-lg font-medium mb-1'>Password</label>
+                            <input
+                                className='border w-[300px] border-[#8c99ab] py-2 px-3 rounded-md text-lg'
+                                type="password"
+                                name="password" 
+                                id=""
+                                placeholder='your Email address' />
+                        </div>
+                        <div>
+                            <button type='submit' className='bg-slate-100 hover:bg-slate-200 py-3 px-4 rounded-md w-[300px] text-lg font-medium'>Log In</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
