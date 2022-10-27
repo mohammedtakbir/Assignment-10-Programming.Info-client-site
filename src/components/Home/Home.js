9c2ff75a36e495d42b73879197020e60a8f69614
mohammedtakbir <takbirhossain678@gmail.com>
2022-10-27 23:23:23 +0600

    more style added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -101,6 +101,42 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            <div className='md:w-[800px] sm:w-[400px] w-[300px] mx-auto md:mt-10 mt-4'>
+                <div className='flex md:flex-row flex-col justify-center items-center gap-5'>
+                    <img className='w-full' src="https://s.udemycdn.com/home/non-student-cta/transform-1x-v3.jpg" alt="" />
+                    <div className=''>
+                        <h3 className='text-3xl font-semibold mb-5'>Transform your life <br /> through education</h3>
+                        <p className='mb-5'>Learners around the world are launching new careers, advancing in their fields, and enriching their lives.</p>
+                        <button className='bg-slate-500 hover:bg-slate-600 md:py-3 py-2 px-4 text-white rounded-md'>Find It Now</button>
+                    </div>
+                </div>
+            </div>
+            <div className='py-8 bg-slate-200 mt-12 text-center'>
+                <div className='container mx-auto'>
+                    <h3 className='text-2xl mb-3 font-semibold'>Trusted by the best</h3>
+                    <p className='mb-6 font-medium'>Leading companies use the same courses to help employees keep their skills fresh.</p>
+                    <ul className='flex flex-wrap md:justify-between lg:mx-16'>
+                        <li className='md:mx-0 mx-3 md:mb-0 mb-3'>
+                            <img src='https://s.udemycdn.com/partner-logos/v4/nasdaq-dark.svg' alt="" />
+                        </li>
+                        <li className='md:mx-0 mx-3 md:mb-0 mb-3'>
+                            <img src='https://s.udemycdn.com/partner-logos/v4/volkswagen-dark.svg' alt="" />
+                        </li>
+                        <li className='md:mx-0 mx-3 md:mb-0 mb-3'>
+                            <img src='https://s.udemycdn.com/partner-logos/v4/box-dark.svg' alt="" />
+                        </li>
+                        <li className='md:mx-0 mx-3 md:mb-0 mb-3'>
+                            <img src='https://s.udemycdn.com/partner-logos/v4/netapp-dark.svg' alt="" />
+                        </li>
+                        <li className='md:mx-0 mx-3 md:mb-0 mb-3'>
+                            <img src='https://s.udemycdn.com/partner-logos/v4/eventbrite-dark.svg' alt="" />
+                        </li>
+                        <li className='md:mx-0 mx-3 md:mb-0 mb-3'>
+                            <img src='https://s.udemycdn.com/partner-logos/v4/tcs-dark.svg' alt="" />
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
